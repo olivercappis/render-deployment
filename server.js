e455ad74dd48f37543d85816feeeaa9468f47b64
Oliver Cappis <Oliver_Cappis1@baylor.edu>
2024-08-01 10:33:22 -0500

    restructured

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const { clog } = require('./middleware/clog.js');
+const api = require('./Unsolved/routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
